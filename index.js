84499d35866e0388bca9a07342755bea5ab55c54
Lucas <lucasvalenteds@gmail.com>
2020-08-01 16:24:12 -0300

    Removing unused function in index.js

--- index.js
+++ index.js
@@ -3,15 +3,11 @@ import App from './src/App';
 
 import PostHog from 'posthog-react-native';
 
-async function main() {
-  await PostHog.setup('cGecHL7Hl_NTJV3FLQhF4hPrPUI_LzLOpRFBnibxVSc', {
-    ios: false,
-    host: 'http://172.17.0.3:31049',
-    captureApplicationLifecycleEvents: true,
-    recordScreenViews: true,
-  });
-}
-
-main();
+PostHog.setup('', {
+  ios: false,
+  host: 'http://172.17.0.3:31049',
+  captureApplicationLifecycleEvents: true,
+  recordScreenViews: true,
+});
 
 AppRegistry.registerComponent('rntemplate', () => App);
